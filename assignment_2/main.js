35675ea9da6c801e2f574d2eee0ec5e1b695e9ae
conumaju <conumaju@users.noreply.github.com>
2020-03-31 15:41:20 -0400

    Update main.js

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -5,12 +5,6 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-<<<<<<< Updated upstream
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
-=======
 for (i = 1; i <= 5; i++) {
     const newImage = document.createElement('img');
     var picture = 'images/pic' + i + '.jpg';
@@ -22,6 +16,5 @@ for (i = 1; i <= 5; i++) {
         displayedImage.src = pict.target.src;
     }
 } 
->>>>>>> Stashed changes
 
 /* Wiring up the Darken/Lighten button  */
